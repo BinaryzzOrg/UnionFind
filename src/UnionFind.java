15bce2191ef141fde88d9a204ed836b8be33e8f1
hachi12h <142139029+hachi12h@users.noreply.github.com>
2024-03-20 09:53:54 +0800

    Update UnionFind.java

--- src/UnionFind.java
+++ src/UnionFind.java
@@ -27,13 +27,18 @@ public class UnionFind {
 	 * that they are already connected and end the method, else if they are trying
 	 * to connect the node to itself end the method also. If no condition were
 	 * satisified in the first if structure proceed to the second where if the first
-	 * node exist in the connected nodes, add the second node to the end, else if
-	 * the second node exist add the first node in the end. Lastly, if both the
-	 * provided node does not exist yet just connect them to each other. The loop
-	 * after the ifs is for keeping track of the connected nodes that are not
-	 * connected yet to the root but connected to each other so when union executes
-	 * it connects both the nodes in the connected roots. Additionally, if the root
-	 * is null just set the first subset as the root.
+	 * node exist in the root and the other exist in the disjoint set traverse to end
+  	 * of the root then connect it the following else if is its opposite where the
+    	 * second node exist in the root and the first node exist in the disjoint subsets
+      	 * if that is the case connect the first node at the end of the root. Moreover,
+	 * the next two else if is for connecting a node that is in the root and a node
+  	 * that doesn't exist in the disjoint subsets meaning that they can just be
+    	 * connected alone at the end of the root. Furthermore, the next else if is
+      	 * that if both provided nodes are from a disjoint set this if condition would
+	 * connect both of them and set the first provided node to be the root and if
+  	 * none of these else if condition are satisified proceed to else where 
+    	 * both nodes would just be connected to each other followed by storing it
+      	 * to the disjoint subsets array.
 	 */
 	public void union(int A, int B) {
 		Node firstNode = find(new Node(A));
@@ -57,7 +62,6 @@ public class UnionFind {
 			return;
 		}
 
-		Node temp = root;
 		if(rootContains(firstNode) && disjointContains(secondNode)) {
 			while(temp.getNext() != null) {
 				temp = temp.getNext();
@@ -72,7 +76,6 @@ public class UnionFind {
 			while(temp.getNext() != null) {
 				temp = temp.getNext();
 			}
-			
 			if(firstNode.getPrev() != null) {
 				temp.setNext(firstNode.getPrev());
 			} else {
@@ -114,27 +117,44 @@ public class UnionFind {
 			firstNode.setNext(secondNode);
 			secondNode.setPrev(firstNode);
 		}
-
+		for(int index = 0; index < subsets.length; index++) {
+			if(subsets[index] == null) {
+				subsets[index] = firstNode;
+				if(root == null) {
+					root = subsets[0];
+				}
+				return;
+			}
+		}
 		//@formatter:off
 			System.out.println("\n"+
 						"⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃\n" +
 						"┇ Elements " + A + " and " + B + " are now connected!\n" +
 						"⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃\n");
 		//@formatter:on
-		
-		for(int index = 0; index < subsets.length; index++) {
-			if(subsets[index] == null) {
-				subsets[index] = firstNode;
-				return;
-			}
-		}
 	}// end method
 
 	/*
-	 * find method returns true if both the given nodes have the same root
+	 * find first checks if the provided data of the user exist in the disjoint subset
+  	 * to confirm if they are connected to each other else check the root
+    	 * if both provided data have the same root return true else return false.
 	 */
 	public boolean find(int A, int B) {
-		return rootContains(new Node(A)) && rootContains(new Node(B));
+		Node firstNode = new Node(A);
+		Node secondNode = new Node(B);
+		
+		for(int index = 0; index < subsets.length; index++) {
+			if(subsets[index] != null && subsets[index].getNext() != null) {
+				if(subsets[index].getData() == firstNode.getData() && subsets[index].getNext().getData() == secondNode.getData()) {
+					return true;
+				}
+				
+				if(subsets[index].getData() == secondNode.getData() && subsets[index].getNext().getData() == firstNode.getData()) {
+					return true;
+				}
+			}
+		}
+		return rootContains(firstNode) && rootContains(secondNode);
 	}// end method
 
 	/*
@@ -149,8 +169,23 @@ public class UnionFind {
 			}
 		}
 		return null;
-	}
+	}//end method
 
+	/*
+ 	 * dijointContains checks the dsjoint subsets array to find a specific node if it exist
+   	 * return true if that specified node is connected to a disjoint subset else false
+ 	 */
+	private boolean disjointContains(Node node) {
+		for(int index = 0; index < subsets.length; index++) {
+			if(subsets[index] != null && subsets[index].getNext() != null) {
+				if(node.getData() == subsets[index].getData() || node.getData() == subsets[index].getNext().getData()) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
 	/*
 	 * rootContains is for checking the main structure of the connected nodes to
 	 * check if a specified node already exist that is connected to the root
